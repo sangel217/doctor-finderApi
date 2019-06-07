6a540a21cd22aa1ccc9357e19309b6fab2a6a9e1
Sarah Angel <sangel217@hotmail.com>
2019-06-07 15:40:20 -0700

    add html for UI

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { DoctorFinder } from './doctor-finer';
+import { DoctorFinder } from './doctor-finder';
 
 $(document).ready(function(){
   $('#searchIssue').click(function(){

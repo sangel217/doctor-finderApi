e1fcd0263ee8f037ba8e9b4149c46d4a1e4ca1d7
Sarah Angel <sangel217@hotmail.com>
2019-06-07 16:30:56 -0700

    add to promise in main.js

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ import './styles.css';
 import { DoctorFinder } from './doctor-finder';
 
 $(document).ready(function(){
-  $('#searchIssue').click(function(){
-      let issue = $('#issue').val();
-      $('#issue').val("");
+  $('.button').click(function(){
+      let issue = $('.issue').val();
+      $('.issue').val("");
 
       let doctorFinder = new DoctorFinder();
       let promise = doctorFinder.getDoctorByIssue(issue);
       promise.then(function(response) {
           let body = JSON.parse(response);
+          $('#showIssue').text(`${body.meta.data}`)
           
-      })
-  })
+      });
+  });
 });
